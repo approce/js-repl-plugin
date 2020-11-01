5754c70b900b0c3a7092e27a44c014d39c4e66f8
GitHub Action <action@github.com>
2020-11-01 13:45:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/approce/jsreplplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/approce/jsreplplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.approce.jsreplplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.approce.jsreplplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/approce/jsreplplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/approce/jsreplplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.approce.jsreplplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.approce.jsreplplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
